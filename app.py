740d34df0f4732221a6b4b77af67028d6d043be0
tarmitpy <assuncaojosetarcisio@gmail.com>
2021-08-19 23:22:05 +0000

    app com banco de dados

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
-from flask import Flask
+import sqlite3
+from flask import Flask, request, session, g, redirect,/ 
+    abort, render_template, flash
 print("Hello world")
-app = Flask(__name__)
 
+# configuração
+DATABASE = "blog.db"
+SECRET_KEY = "pudim"
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+def conectar_bd()
+    return sqlite3.connect(app.config['DATABASE'])
+
+    
 @app.route('/hello') # decoretor do Flask
 def pagina_inicial():
     return "Hello World"
